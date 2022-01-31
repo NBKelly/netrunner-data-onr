345905741ca06e4f87902705220458c3c73259c6
NB Kelly <N.B.Kelly@protonmail.com>
2022-01-31 15:36:58 +1300

    updated setup

--- onr_setup.sh
+++ onr_setup.sh
@@ -6,6 +6,11 @@ if test -f "onr_out/stage_one.txt"; then
 else
     mkdir onr_out
     echo "fetching from Emergency Shutdown"
-    python3 scrape.py > onr_out/stage_one.txt
+    python3 onr_setup/scrape.py > onr_out/stage_one.txt
 fi
-   
+
+#check the cycles entry
+onr_setup/insert_cycles.sh
+
+#check the factions entry
+onr_setup/insert_factions.sh
