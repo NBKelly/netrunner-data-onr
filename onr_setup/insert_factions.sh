58898bc9c3d0460dfdce5a0abf19f0c06891604e
NB Kelly <N.B.Kelly@protonmail.com>
2022-01-31 15:30:06 +1300

    formatting

--- onr_setup/insert_factions.sh
+++ onr_setup/insert_factions.sh
@@ -4,7 +4,7 @@
 
 if ! grep -q "onr_" "edn/factions.edn"; then
     echo "didn't find ONR entries - altering factions.edn file"
-    FILE=`cat edn/factions.edn`
+    FILE=$(<edn/factions.edn)
     FILE=${FILE%?}
     APPEND=`cat onr_setup/factions.edn`
     OUTPUT=$FILE$APPEND
